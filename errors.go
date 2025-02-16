4a221ce00642e69643fa0ff177273b41172f83f3
Felixheuer <fheuer14@gmail.com>
2025-02-16 19:54:45 +0100

    change to german

--- errors.go
+++ errors.go
@@ -3,12 +3,12 @@ package validator
 import "errors"
 
 var (
-	errStringToInt          = errors.New("validator: unable to parse string to integer")
-	errStringToFloat        = errors.New("validator: unable to parse string to float")
-	errRequireRules         = errors.New("validator: provide at least rules for Validate* method")
-	errValidateArgsMismatch = errors.New("validator: provide at least *http.Request and rules for Validate method")
-	errInvalidArgument      = errors.New("validator: invalid number of argument")
-	errRequirePtr           = errors.New("validator: provide pointer to the data structure")
-	errRequireData          = errors.New("validator: provide non-nil data structure for ValidateStruct method")
-	errRequestNotAccepted   = errors.New("validator: cannot provide an *http.Request for ValidateStruct method")
+	errStringToInt          = errors.New("validator: Die Zeichenkette konnte nicht in eine Ganzzahl umgewandelt werden")
+	errStringToFloat        = errors.New("validator: Die Zeichenkette konnte nicht in eine Gleitkommazahl umgewandelt werden")
+	errRequireRules         = errors.New("validator: Bitte geben Sie mindestens eine Regel für die Validate*-Methode an")
+	errValidateArgsMismatch = errors.New("validator: Es müssen mindestens eine *http.Request und Regeln für die Validate-Methode übergeben werden")
+	errInvalidArgument      = errors.New("validator: Ungültige Anzahl an Argumenten übergeben")
+	errRequirePtr           = errors.New("validator: Die Datenstruktur muss als Zeiger übergeben werden")
+	errRequireData          = errors.New("validator: Bitte geben Sie eine nicht-leere Datenstruktur für die ValidateStruct-Methode an")
+	errRequestNotAccepted   = errors.New("validator: Die ValidateStruct-Methode akzeptiert keine *http.Request-Objekte")
 )
